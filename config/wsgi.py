4cf59b8cdd5a449f722d5d14f85a1573918bf875
anderson89marques <andersonoanjo18@gmail.com>
2023-10-09 14:12:00 -0300

    Initial Structure to the project.

---
+++ config/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for config project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+app_path = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+)
+sys.path.append(os.path.join(app_path, "text_translator"))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = get_wsgi_application()
